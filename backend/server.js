1fccd62eb272d9b63b0599f2a6316279f57af406
Raghav Verma <130084894+Raghavcpp@users.noreply.github.com>
2023-12-04 20:07:02 +0000

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Allow all origins for simplicity in this example.
+// In a production environment, you may want to specify only trusted origins.
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/message', (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
+app.listen(8000, () => {
+    console.log(`Server is running on port 8000.`);
+});
